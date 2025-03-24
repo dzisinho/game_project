63eb15c8c394c21b7aadaf4b46993b7afa6ade85
dzisinho <dzisbogdan26@gmail.com>
2025-03-24 20:20:23 +0200

    Initial commit

---
+++ Application/Game.cpp
@@ -0,0 +1,26 @@
+﻿#include "Game.h"
+#include <iostream>
+using namespace std;
+
+void battle(Player& player, Enemy& enemy) {
+    cout << "A " << enemy.getName() << " appears!" << endl;
+    while (player.isAlive() && enemy.isAlive()) {
+        player.attackTarget(enemy);
+        if (enemy.isAlive()) {
+            enemy.attackTarget(player);
+        }
+        if (player.isAlive()) {
+            cout << "Do you want to use an item? (y/n): ";
+            char choice;
+            cin >> choice;
+            if (choice == 'y') {
+                player.useItem();
+            }
+        }
+    }
+    if (player.isAlive()) {
+        cout << "You defeated the " << enemy.getName() << "!" << endl;
+    } else {
+        cout << "You were defeated... Game Over!" << endl;
+    }
+}
